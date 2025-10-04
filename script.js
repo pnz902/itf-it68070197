cc2d86b5338783c9ed55e67208d87fd179754b49
pnz902 <ananyayaya.ay07@gmail.com>
2025-10-05 02:48:54 +0700

    not make same column (back)

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
-const myinput = document.getElementById("myinput")
 const account = document.getElementById("account")
 const cash = document.getElementById("cash")
-const selectBox = document.getElementById("bank-operation");
-
-function add() {
-    myinput.value = Number(myinput.value) + 1
-}
+const operate = document.getElementById("bank-operations")
+const money = document.getElementById("money")
+let count = 0
 
 function change() {
     account.value = Number(account.value)
     cash.value = Number(cash.value)
 }
 
-function result() {
-
+function showresult() {
+    count = Number(count) + 1
+    if (operate == 'Deposit'){
+        if (money <= cash){
+            account.value = Number(account.value) + Number(money)
+            cash.value = Number(cash.value) - Number(money)
+            document.getElementById("result").innerHTML = ("%d, Current account balance: %d, Current cash balance: %d",count,account,cash)
+        }else {
+            document.getElementById("result").innerHTML = ("%d, Couldn't deposit entered balance. (Insufficient cash balance)",count)
+        }
+    }else {
+        if (money <= account){
+            cash.value = Number(cash.value) + Number(money)
+            account.value = Number(account.value) - Number(money)
+            document.getElementById("result").innerHTML = ("%d, Current account balance: %d, Current cash balance: %d",count,account,cash)
+        }else {
+            document.getElementById("result").innerHTML = ("%d, Couldn't withdraw entered balance. (Insufficient account balance)",count)
+        }
+    }
 }
