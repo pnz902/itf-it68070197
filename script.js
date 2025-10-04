d53e004a3bbce63418f7c2ecc3621fd43dfb990d
pnz902 <ananyayaya.ay07@gmail.com>
2025-10-04 15:53:36 +0700

    make text space between

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const myinput = document.getElementById("myinput")
 const account = document.getElementById("account")
 const cash = document.getElementById("cash")
+const selectBox = document.getElementById("bank-operation");
 
 function add() {
     myinput.value = Number(myinput.value) + 1
@@ -9,4 +10,8 @@ function add() {
 function change() {
     account.value = Number(account.value)
     cash.value = Number(cash.value)
+}
+
+function result() {
+
 }
