3452d67112a7050e671e440f46be12ed0c98d22b
pnz902 <ananyayaya.ay07@gmail.com>
2025-10-05 17:16:20 +0700

    finish lab13

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ let count = 0
 function change() {
     count = Number(count) + 1
     if (result.value != ""){
-        result.value = (count + ", Current account balance: " + account.value + ", Current cash balance: " + cash.value + "\n") + result.value
+        result.value = (count + ",  Current account balance:  " + account.value + ", Current cash balance:  " + cash.value + "\n") + result.value
     }else{
-        result.value = (count + ", Current account balance: " + account.value + ", Current cash balance: " + cash.value)
+        result.value = (count + ",  Current account balance:  " + account.value + ", Current cash balance:  " + cash.value)
     }
 }
 
@@ -20,17 +20,17 @@ function showresult() {
         if (Number(money.value) <= Number(cash.value)){
             account.value = Number(account.value) + Number(money.value)
             cash.value = Number(cash.value) - Number(money.value)
-            result.value = (count + ", Current account balance: " + account.value + ", Current cash balance: " + cash.value + "\n") + result.value
+            result.value = (count + ",  Current account balance:  " + account.value + ", Current cash balance:  " + cash.value + "\n") + result.value
         }else {
-            result.value =  count + ", Couldn't deposit entered balance. (Insufficient cash balance)" + "\n" + result.value}
+            result.value =  count + ",  Couldn't deposit entered balance. (Insufficient cash balance)" + "\n" + result.value}
         }
     else{
         if (Number(money.value) <= Number(account.value)){
             cash.value = Number(cash.value) + Number(money.value)
             account.value = Number(account.value) - Number(money.value)
-            result.value = (count + ", Current account balance: " + account.value + ", Current cash balance: " + cash.value + "\n") + result.value
+            result.value = (count + ",  Current account balance:  " + account.value + ", Current cash balance:  " + cash.value + "\n") + result.value
         }else {
-            result.value = count + ", Couldn't withdraw entered balance. (Insufficient account balance)" + "\n" + result.value
+            result.value = count + ",  Couldn't withdraw entered balance. (Insufficient account balance)" + "\n" + result.value
         }
     }
 }
